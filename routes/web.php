e581be0414783a5e855d2ee0ccb5d2c3d85ce76e
Isai Rodriguez <macover1507@hotmail.com>
2022-11-15 16:46:41 -0600

    login added and navbar with routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth')->group(function () {
+
+    /* Companies */
+    Route::get('/companies', [CompanyController::class, 'index'])->name('companies');
+
+    /* Employees */
+    Route::get('/employees', [EmployeeController::class, 'index'])->name('employees');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
